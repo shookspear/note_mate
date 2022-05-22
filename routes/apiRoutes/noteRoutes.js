b4897929e715ce0d78a8741e09870706c357c0c5
Echeta Okeke <echeta@rogers.com>
2022-05-22 01:07:05 -0400

    added base route files

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const {
+  filterByQuery,
+  findById,
+  createNewNote,
+  validateNote
+} = require('../../lib/notes');
+const { notes } = require('../../data/notes');
+
+router.get('/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
