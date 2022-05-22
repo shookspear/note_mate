883c542932e22d15d8a34ed49556ff3d040e57b2
Echeta Okeke <echeta@rogers.com>
2022-05-22 02:49:39 -0400

    troubleshooting routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,37 +1,22 @@
 const router = require('express').Router();
-const {
-  filterByQuery,
-  findById,
-  createNewNote,
-  validateNote
-} = require('../../lib/notes');
-const { notes } = require('../../data/notes');
+const { getNotes, createNewNote } = require('../../lib/notes');
+const notesData = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
+  const results = getNotes();
   res.json(results);
 });
 
-router.get('/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
+
 
 router.post('/notes', (req, res) => {
-  req.body.id = notes.length.toString();
+  req.body.id = notesData.length.toString();
 
   if (!validateNote(req.body)) {
     res.status(400).send('The note is not properly formatted.');
   } else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
   }
 });
 
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
 
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
